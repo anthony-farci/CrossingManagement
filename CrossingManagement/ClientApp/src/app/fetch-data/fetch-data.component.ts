d9e6834bb379649dc4a57d6537879598d918da27
anthony <anthonyfarci.freelance@gmail.com>
2023-05-08 16:12:14 +0200

    Railroad crossings names are displayed in fetch data

--- CrossingManagement/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ CrossingManagement/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,19 +6,28 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public railroadCrossings: RailroadCrossing[] = [];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+  constructor(http: HttpClient) {
+    http.get<RailroadCrossing[]>("https://opendata.infrabel.be/api/v2/catalog/datasets/geoow/exports/json?limit=-1&offset=0&timezone=UTC").subscribe(result => {
+      this.railroadCrossings = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface RailroadCrossing {
+  fld_naam_ramses: string;
+  fld_actief_passief: string;
+  type_pn: string;
+  fld_geo_x: string;
+  fld_geo_y: string;
+  fld_postcode_en_gemeente: string;
+  position_du_passage_a_niveau: RailroadCrossingPosition;
+  type_lc: string;
+}
+
+interface RailroadCrossingPosition {
+  lon: number;
+  lat: number;
 }
 
