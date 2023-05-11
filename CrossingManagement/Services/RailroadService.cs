a16c8cc4eca1224535bc293f9c1760faf071f950
anthony <anthonyfarci.freelance@gmail.com>
2023-05-11 22:12:26 +0200

    RailroadService.cs : Added Create and Delete methods

---
+++ CrossingManagement/Services/RailroadService.cs
@@ -0,0 +1,38 @@
+using CrossingManagement.Models;
+using CrossingManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrossingManagement.Service;
+
+public class RailroadService
+{
+
+    private readonly RailroadContext _context;
+
+    public RailroadService(RailroadContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<RailroadCrossing> GetAllRailroadCrossings()
+    {
+        return _context.RailroadCrossings.AsNoTracking().ToList();
+    }
+
+    public RailroadCrossing Create(RailroadCrossing newRailroadCrossing)
+    {
+        _context.RailroadCrossings.Add(newRailroadCrossing);
+        _context.SaveChanges();
+
+        return newRailroadCrossing;
+    }
+    public void Delete(int id)
+    {
+        var railroadToDelete = _context.RailroadCrossings.Find(id);
+        if (railroadToDelete is not null)
+        {
+            _context.RailroadCrossings.Remove(railroadToDelete);
+            _context.SaveChanges();
+        }
+    }
+}
